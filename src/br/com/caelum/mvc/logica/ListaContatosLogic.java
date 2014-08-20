b206d02b6f36389ed478640befe58d246c1bf92c
Juliano Ramos <490meister@gmail.com>
2014-08-20 13:17:14 -0300

    Capitulo 10, Filter

--- src/br/com/caelum/mvc/logica/ListaContatosLogic.java
+++ src/br/com/caelum/mvc/logica/ListaContatosLogic.java
@@ -1,5 +1,6 @@
 package br.com.caelum.mvc.logica;
 
+import java.sql.Connection;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,8 +14,10 @@ public class ListaContatosLogic implements Logica {
 	@Override
 	public String executa(HttpServletRequest req, HttpServletResponse res)
 			throws Exception {
+		
+		Connection connection = (Connection) req.getAttribute("conexao");
 
-		List<Contato> contatos = new ContatoDao().getLista();
+		List<Contato> contatos = new ContatoDao(connection).getLista();
 		
 		req.setAttribute("contatos", contatos);
 		
